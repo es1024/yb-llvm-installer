ac0d2341145af630076729278ba8122124308693
Eric Sheng <esheng@yugabyte.com>
2026-06-16 08:16:01 -0700

    test libunwind fix

--- src/llvm_installer/__init__.py
+++ src/llvm_installer/__init__.py
@@ -27,7 +27,7 @@ from typing import Optional, List, Union, Sequence, Tuple, Dict
 
 ARCH_RE_STR = '|'.join(['x86_64', 'aarch64', 'arm64'])
 
-DEFAULT_LLVM_GITHUB_RELEASE_URL_PREFIX = 'https://github.com/yugabyte/build-clang/releases/download'
+DEFAULT_LLVM_GITHUB_RELEASE_URL_PREFIX = 'https://github.com/es1024/yb-build-clang/releases/download'
 DEFAULT_LLVM_PACKAGE_NAME_PREFIX = 'yb-llvm-'
 DEFAULT_GCC_GITHUB_RELEASE_URL_PREFIX = 'https://github.com/yugabyte/build-gcc/releases/download'
 DEFAULT_GCC_PACKAGE_NAME_PREFIX = 'yb-gcc-'
